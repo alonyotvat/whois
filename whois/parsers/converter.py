c49f21a5e8616c1220fb1fe5aecbc3d2a87d5db4
alonyotvat <34003415+alonyotvat@users.noreply.github.com>
2018-05-03 19:26:23 +0300

    skip dates with parsed year < 1970

--- whois/parsers/converter.py
+++ whois/parsers/converter.py
@@ -24,6 +24,9 @@ class DateRegex:
 
         try:
             year = match.group('year')
+            if year < 1970:
+                return None
+
             month = match.group('month')
             day = match.group('day')
         except Exception:
